50cf97544c26133abe3893e75cacc863c682fe12
Chris Ray <30697743+cjray14@users.noreply.github.com>
2020-11-08 10:23:12 +0000

    Simple megamenu and search splash screens

---
+++ src/scripts/modules/search.js
@@ -0,0 +1,19 @@
+export default function init({el}) {
+
+  /* The original element that requested the module */
+  const context = el;
+
+  /* Context sensitive things to target */
+  const elements = {
+    targets: document.querySelectorAll('[data-search-toggle]'),
+  };
+
+  /* Loop through the elements */
+  elements.targets.forEach((element) => {
+    element.addEventListener('click', (event) => {
+      event.preventDefault();
+
+      document.body.classList.toggle('lay-Search-active');
+    });
+  });
+}
